4a00eb517655c9e73e11f04b72a910e67dc3f5aa
user <user@WS-1C5BEF2AFE.belinvestbank.by>
2022-05-28 17:26:59 +0300

    feat: implement toggle by dbclick

--- angular-todo/src/app/components/task-item/task-item.component.ts
+++ angular-todo/src/app/components/task-item/task-item.component.ts
@@ -5,19 +5,23 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: ITasks;
-  @Output() onDeleteTask: EventEmitter<ITasks> = new EventEmitter()
+  @Output() onDeleteTask: EventEmitter<ITasks> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<ITasks> = new EventEmitter();
   faTimes = faTimes;
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   onDelete(task: ITasks) {
-    this.onDeleteTask.emit(task)
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: ITasks) {
+    this.onToggleReminder.emit(task);
   }
 }
--- angular-todo/src/app/services/task.service.ts
+++ angular-todo/src/app/services/task.service.ts
@@ -4,20 +4,31 @@ import  {ITasks } from '../ITask'
 
 import { Injectable } from '@angular/core';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks'
+  private apiUrl = 'http://localhost:5000/tasks';
 
   constructor(private http: HttpClient) {}
 
-  getTasks(): Observable<Array<ITasks>>{
+  getTasks(): Observable<Array<ITasks>> {
     return this.http.get<Array<ITasks>>(this.apiUrl);
   }
 
   deleteTask(task: ITasks): Observable<ITasks> {
-    const url = `${this.apiUrl}/${task.id}`
-    return this.http.delete<ITasks>(url)
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<ITasks>(url);
+  }
+
+  updateTaskReminder(task: ITasks) {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<ITasks>(url, task, httpOptions);
   }
 }
